0125baaf55e60746c7bce126f39a0c20c7f9218a
Abhinav <abhinavkumar.sieger@gmail.com>
2018-07-01 08:47:05 +0530

    First Visualization Ready. Beautified and tested.

--- GSOC18/Visualization_Module/prediction_visualization.py
+++ GSOC18/Visualization_Module/prediction_visualization.py
@@ -37,26 +37,98 @@ def plot_histogram(predictions,labels):
     predictions=predictions[perm,:]
     labels=labels[perm,:]
 
+    #Plotting the error histogram in Energy
+    plt.hist(predictions[:,0]-labels[:,0],ec='k',alpha=0.7,bins=100)
+    plt.title('Energy Error Histogram: [prediction-labels] ')
+    plt.xlabel('Energy Error')
+    plt.ylabel('Counts (out of total {} test samples)'.format(labels.shape[0]))
+    plt.show()
+    plt.close()
+    #Error histogram in Eta prediction
+    plt.hist(predictions[:,1]-labels[:,1],ec='k',alpha=0.7,bins=100)
+    plt.title('Eta Error Histogram: [prediction-labels] ')
+    plt.xlabel('Eta Error')
+    plt.ylabel('Counts (out of total {} test samples)'.format(labels.shape[0]))
+    plt.show()
+    plt.close()
+    #Error histogram in Phi prediction
+    plt.hist(predictions[:,2]-labels[:,2],ec='k',alpha=0.7,bins=100)
+    plt.title('Phi Error Histogram: [prediction-labels] ')
+    plt.xlabel('Phi Error')
+    plt.ylabel('Counts (out of total {} test samples)'.format(labels.shape[0]))
+    plt.show()
+    plt.close()
+
     #Taking the random shuffled sample
     no_samples=100
+    x=range(1,no_samples+1)
     predictions=predictions[0:no_samples,:]
     labels=labels[0:no_samples,:]
 
-    #Plotting these sample
-    x=range(1,no_samples+1)
-    plt.bar(x,predictions[:,0],color='#ff9933')
-    plt.bar(x,labels[:,0],alpha=0.5,color='g')
+    #Plotting these sample : Energy
+    fig=plt.figure()
+    fig.suptitle('Energy Prediction:Label')
+    #Plotting the overalyed Bar Graph
+    ax1=fig.add_subplot(211)
+    ax1.bar(x,predictions[:,0],alpha=1,color='#ff9933',label='prediction')
+    ax1.bar(x,labels[:,0],alpha=0.5,color='b',label='label')
+    ax1.set_xlabel('{} random sample prediction and labels'.format(no_samples))
+    ax1.set_ylabel('Value')
+    ax1.legend()
+    ax1.set_title('Bar Graph: Prediction and Label overlayed')
+    #Plotting the corresponding difference
+    ax2=fig.add_subplot(212)
+    ax2.bar(x,predictions[:,0]-labels[:,0],alpha=0.7,color='r',label='Error')
+    ax2.set_xlabel('Same random Sample as above')
+    ax2.set_ylabel('Error')
+    ax2.legend()
+    ax2.set_title('Prediction-Label')
     plt.show()
+    plt.close()
 
-    plt.clf()
-    plt.bar(x,predictions[:,1],color='#ff9933')
-    plt.bar(x,labels[:,1],alpha=0.5,color='g')
-    plt.show()
 
-    plt.clf()
-    plt.bar(x,predictions[:,2],color='#ff9933')
-    plt.bar(x,labels[:,2],alpha=0.5,color='g')
+    #Plotting the samples : Eta
+    fig=plt.figure()
+    fig.suptitle('Eta Prediction:Label')
+    #Plotting the overalyed Bar Graph
+    ax1=fig.add_subplot(211)
+    ax1.bar(x,predictions[:,1],alpha=1,color='#ff9933',label='prediction')
+    ax1.bar(x,labels[:,1],alpha=0.5,color='b',label='label')
+    ax1.set_xlabel('{} random sample prediction and labels'.format(no_samples))
+    ax1.set_ylabel('Value')
+    ax1.legend()
+    ax1.set_title('Bar Graph: Prediction and Label overlayed')
+    #Plotting the corresponding difference
+    ax2=fig.add_subplot(212)
+    ax2.bar(x,predictions[:,1]-labels[:,1],alpha=0.7,color='r',label='Error')
+    ax2.set_xlabel('Same random Sample as above')
+    ax2.set_ylabel('Error')
+    ax2.legend()
+    ax2.set_title('Prediction-Label')
     plt.show()
+    plt.close()
+
+    #Plotting the samples :Phi
+    fig=plt.figure()
+    fig.suptitle('Phi Prediction:Label')
+    #Plotting the overalyed Bar Graph
+    ax1=fig.add_subplot(211)
+    ax1.bar(x,predictions[:,2],alpha=1,color='#ff9933',label='prediction')
+    ax1.bar(x,labels[:,2],alpha=0.5,color='b',label='label')
+    ax1.set_xlabel('{} random sample prediction and labels'.format(no_samples))
+    ax1.set_ylabel('Value')
+    ax1.legend()
+    ax1.set_title('Bar Graph: Prediction and Label overlayed')
+    #Plotting the corresponding difference
+    ax2=fig.add_subplot(212)
+    ax2.bar(x,predictions[:,2]-labels[:,2],alpha=0.7,color='r',label='Error')
+    ax2.set_xlabel('Same random Sample as above')
+    ax2.set_ylabel('Error')
+    ax2.legend()
+    ax2.set_title('Prediction-Label')
+    plt.show()
+    plt.close()
+
 
 
 if __name__=='__main__':
