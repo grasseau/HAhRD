a45922354ae1983e7b06f7ea1a27b3d081aae0fc
Abhinav <abhinavkumar.sieger@gmail.com>
2018-07-02 22:25:05 +0530

    Resotred the cost function and accuracy calculation as per the model 1 and model 2 ie. 5 output. Made a separeate script for the model with just the energy prediction as regression.

--- GSOC18/model3_definition.py
+++ GSOC18/model3_definition.py
@@ -32,24 +32,25 @@ def calculate_model_accuracy(Z,Y):
         tf.summary.scalar('percentage_energy_error',energy_error)
 
         #Eta Accuracy/Error
-        eta_abs_diff=tf.losses.absolute_difference(Z[:,1],
-                                    Y[:,1],reduction=tf.losses.Reduction.NONE)
-        eta_error=(tf.reduce_mean(tf.abs(tf.divide(
-                                    eta_abs_diff,Y[:,1]+1e-10))))*100
-        tf.summary.scalar('percentage_eta_error',eta_error)
+        # eta_abs_diff=tf.losses.absolute_difference(Z[:,1],
+        #                             Y[:,1],reduction=tf.losses.Reduction.NONE)
+        # eta_error=(tf.reduce_mean(tf.abs(tf.divide(
+        #                             eta_abs_diff,Y[:,1]+1e-10))))*100
+        # tf.summary.scalar('percentage_eta_error',eta_error)
 
         #Phi Accuracy/Error
-        phi_abs_diff=tf.losses.absolute_difference(Z[:,2],
-                                    Y[:,2],reduction=tf.losses.Reduction.NONE)
-        phi_error=(tf.reduce_mean(tf.abs(tf.divide(
-                                    phi_abs_diff,Y[:,2]+1e-10))))*100
-        tf.summary.scalar('percentage_phi_error',phi_error)
+        # phi_abs_diff=tf.losses.absolute_difference(Z[:,2],
+        #                             Y[:,2],reduction=tf.losses.Reduction.NONE)
+        # phi_error=(tf.reduce_mean(tf.abs(tf.divide(
+        #                             phi_abs_diff,Y[:,2]+1e-10))))*100
+        # tf.summary.scalar('percentage_phi_error',phi_error)
 
 
         #Calculation of Classification error
-        regression_len=3
+        regression_len=1        #from where classification label starts in prediction
+        regression_label_len=3  #from where classification label starts in labels
         prediction=tf.argmax(Z[:,regression_len:],axis=1)
-        label=tf.argmax(Y[:,regression_len:],axis=1)
+        label=tf.argmax(Y[:,regression_label_len:],axis=1)
 
         correct=tf.equal(prediction,label)
         classification_accuracy=tf.reduce_mean(tf.cast(correct,'float'))*100
@@ -57,8 +58,6 @@ def calculate_model_accuracy(Z,Y):
 
     #Returning the accuracy tuple to be used in inference module
     accuracy_tuple=(energy_error,
-                    eta_error,
-                    phi_error,
                     classification_accuracy)
     return accuracy_tuple
 
@@ -101,7 +100,7 @@ def calculate_total_loss(Z,Y,scope=None):
 
         #Calculating the model loss
         #Calculating the regression loss(scalar)
-        regression_len=3
+        regression_len=1
         regression_output=Z[:,0:regression_len]#0,1,2
         regression_label=Y[:,0:regression_len]#TRUTH
 
@@ -120,8 +119,9 @@ def calculate_total_loss(Z,Y,scope=None):
         all_loss_list.append(regression_loss)
 
         #Calculating the x-entropy loss(scalar)
+        regression_label_len=3
         class_output=Z[:,regression_len:]#3,4,....
-        class_label=Y[:,regression_len:]#TRUTH
+        class_label=Y[:,regression_label_len:]#TRUTH
         categorical_loss=tf.losses.softmax_cross_entropy(class_label,
                                                         class_output)
         tf.summary.scalar('categorical_loss',categorical_loss)
@@ -133,153 +133,7 @@ def calculate_total_loss(Z,Y,scope=None):
 
     return total_loss
 
-
-def model1(X,is_training):
-    '''
-    DESCRIPTION:
-        Defining the first CNN model for the HGCAL. This function will
-        take the input X and apply the CNN as defined in this model
-        and give the unrectified output of the last layer as output
-        for the computing the cost/error for training.
-
-    USAGE:
-        INPUT:
-            X           : the input "image" to the model
-            is_training : the training flag to know whether we are in
-                            training mode or not which is required
-                            for batch norm and dropout layers to modify
-                            thier behaviour accordingly.
-        OUTPUT:
-            Z           : the output (un rectified output) of this
-                            model to furthur compute the loss and hence
-                            gradient.
-    '''
-    #Model Hyperparameter Section
-    #(A separate handling mechanism will be developed for this later)
-    bn_decision=False           #Batch-Normalization (ON/OFF)
-    lambd=0.0                   #control L2-Regularization of weights
-    dropout_rate=0.0            #The dropout rates of activation in any layer
-
-
-    #Reshaping the input to appropriate format
-    X_img=tf.expand_dims(X,axis=-1,name='channel_dim')
-
-    #Passing it through the first layer
-    A1=rectified_conv3d(X_img,
-                        name='conv3d1',
-                        filter_shape=(3,3,3),
-                        output_channel=5,
-                        stride=(1,1,1),
-                        padding_type='VALID',
-                        is_training=is_training,
-                        dropout_rate=dropout_rate,
-                        apply_batchnorm=bn_decision,
-                        weight_decay=lambd,
-                        apply_relu=True)#it is default no need to write
-    A1Mp=max_pooling3d(A1,
-                        name='mpool1',
-                        filter_shape=(3,3,3),
-                        stride=(2,2,2),
-                        padding_type='VALID')
-
-    #Passing it through the second layer
-    A2=rectified_conv3d(A1Mp,
-                        name='conv3d2',
-                        filter_shape=(3,3,3),
-                        output_channel=10,
-                        stride=(1,1,1),
-                        padding_type='VALID',
-                        is_training=is_training,
-                        dropout_rate=dropout_rate,
-                        apply_batchnorm=bn_decision,
-                        weight_decay=lambd,
-                        apply_relu=True)#it is default no need to write
-    A2Mp=max_pooling3d(A2,
-                        name='mpool2',
-                        filter_shape=(3,3,3),
-                        stride=(2,2,2),
-                        padding_type='VALID')
-
-    #Writing the third layer similar as the above pattern
-    A3=rectified_conv3d(A2Mp,
-                        name='conv3d3',
-                        filter_shape=(3,3,3),
-                        output_channel=10,
-                        stride=(1,1,1),
-                        padding_type='VALID',
-                        is_training=is_training,
-                        dropout_rate=dropout_rate,
-                        apply_batchnorm=bn_decision,
-                        weight_decay=lambd,
-                        apply_relu=True)#it is default no need to write
-    A3Mp=max_pooling3d(A3,
-                        name='mpool3',
-                        filter_shape=(3,3,3),
-                        stride=(2,2,2),
-                        padding_type='VALID')
-
-    #Passing the activation to the fourth layer which is identity
-    A4=identity3d_residual_block(A3Mp,
-                                name='identity1',
-                                num_channels=[2,2,10],
-                                mid_filter_shape=(3,3,3),
-                                is_training=is_training,
-                                dropout_rate=dropout_rate,
-                                apply_batchnorm=bn_decision,
-                                weight_decay=lambd)
-    A4Mp=max_pooling3d(A4,
-                        name='mpool4',
-                        filter_shape=(3,3,3),
-                        stride=(3,3,1),
-                        padding_type='SAME')
-
-    #Passing through the fifth layer
-    A5=convolutional3d_residual_block(A4Mp,
-                                        name='conv_res1',
-                                        num_channels=[2,2,15],
-                                        first_filter_stride=(1,1,1),
-                                        mid_filter_shape=(3,3,3),
-                                        is_training=is_training,
-                                        dropout_rate=dropout_rate,
-                                        apply_batchnorm=bn_decision,
-                                        weight_decay=lambd)
-    A5Mp=max_pooling3d(A5,
-                        name='mpool5',
-                        filter_shape=(3,3,3),
-                        stride=(3,3,1),
-                        padding_type='SAME')
-
-    #Passing it through the sixth layer
-    #Passing the activation to the fourth layer which is identity
-    A6=identity3d_residual_block(A5Mp,
-                                name='identity2',
-                                num_channels=[2,2,15],
-                                mid_filter_shape=(3,3,3),
-                                is_training=is_training,
-                                dropout_rate=dropout_rate,
-                                apply_batchnorm=bn_decision,
-                                weight_decay=lambd)
-    A6Mp=max_pooling3d(A6,
-                        name='mpool6',
-                        filter_shape=(3,3,3),
-                        stride=(3,3,1),
-                        padding_type='SAME')
-
-    #Now finally flattening it and connecting it with fully
-    #connected layer
-    Z7=simple_fully_connected(A6Mp,
-                                name='fc1',
-                                output_dim=5,
-                                is_training=is_training,
-                                dropout_rate=dropout_rate,
-                                apply_batchnorm=False,#remember regression
-                                weight_decay=lambd,
-                                flatten_first=True,
-                                apply_relu=False)
-
-    return Z7
-
-def model2(X,is_training):
+def model3(X,is_training):
     '''
     DESCRIPTION:
         This model is based on the importsnce of global view
@@ -290,14 +144,8 @@ def model2(X,is_training):
         1.Energy: since the energy of particle is left in the whole
                     depth as the particle penetrates the depth of
                     detector.
-        2.Phi   : Again, this signifies the orientation of the
-                    momentum/trajectory direction in the x-y plane
-                    and means same as phi of polar coordiate.
-        3.Eta   : (pseudorapidity) is the angle that the momentum
-                    vector or trajectory makes with the z axis
-                    transformed by tan and log function. So,
-                    ulitmately it is dependent on the whole trajectory
-                    information
+        This function is very similar to the model2 but this
+        will only do the regression on the energy output
     USAGE:
         INPUT:
             X           : the input hits "image" to the model
@@ -421,7 +269,7 @@ def model2(X,is_training):
     #Finally the prediction/output layer
     Z7=simple_fully_connected(A6,
                                 name='fc2',
-                                output_dim=5,
+                                output_dim=3,
                                 is_training=is_training,
                                 dropout_rate=dropout_rate,
                                 apply_batchnorm=False,
